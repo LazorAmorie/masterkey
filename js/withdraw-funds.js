a13c2e9b63f954ac8d0b121abe76ab8e66e0ef4d
lazoramorie <georgehssein>
2025-04-02 20:12:03 +0200

    fix: move methodOptions hiding logic inside method click event

--- js/withdraw-funds.js
+++ js/withdraw-funds.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    methodOptions.forEach(option => option.style.display = 'none'); // Hides it initially 
+   
     const methods = document.querySelectorAll('.method');
     const methodOptions = document.querySelectorAll('.method-options');
 
     methods.forEach(method => {
+        methodOptions.forEach(option => option.style.display = 'none'); // Hides it initially 
         method.addEventListener('click', () => {
             methodOptions.forEach(option => option.style.display = 'none');
             const selectedMethod = method.getAttribute('data-method');
