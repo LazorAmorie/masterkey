50f369fb9e73a112cb6fbd823bb82228caf343e2
lazoramorie <georgehssein>
2025-04-29 18:54:26 +0200

    this what i di dnt commit when i went on vacation

--- masterkey-backend/index.js
+++ masterkey-backend/index.js
@@ -1,28 +1,21 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
-
-dotenv.config();
-
+const mangoose = required("mangoose");
 const app = express();
-app.use(cors());
-app.use(express.json());
+const port = 3000;
 
-// Test route
+// Example
 app.get('/', (req, res) => {
-  res.send('Hello, Masterkey!');
+  res.send('Welcome to the MasterKey Backend API Updated For u!');
 });
 
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('✅ MongoDB Connected'))
-  .catch((err) => console.error('❌ MongoDB connection error:', err));
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`⚡ Server is running on port ${PORT}`);
+mongoose.connect("mongodb+srv://georgehssein:12/jan/5002@masterkeydb.ghpt6cm.mongodb.net/Masterkey-API?retryWrites=true&w=majority&appName=MasterKeyDB")
+.then (()  => {
+  console.log("Connected to MongoDB Atlas");
+})
+.catch((error) => {
+  console.log("connection failed");
 });
